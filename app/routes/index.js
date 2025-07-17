ac7a032613cc901f2140046af0d51ad568827d18
Quentin Hacourt <quentin.hacourt@gmail.com>
2025-07-17 16:49:37 +0200

    secret message for user

--- app/routes/index.js
+++ app/routes/index.js
@@ -5,20 +5,14 @@ import { action } from '@ember/object';
 export default class IndexRoute extends Route {
   @service store;
   @service currentSession;
-  @service trending;
 
   async model() {
     const currentUserAccount = this.currentSession.signedInPerson;
     const posts = await this.store.findAll('post', { include: ['author'] });
-    await this.trending.load();
-    const mostRecent = this.trending.mostRecentPost;
-    const mostPopular = this.trending.mostPopularPost;
 
     const dataForTemplate = {
       posts: posts,
       account: currentUserAccount,
-      mostRecent: mostRecent,
-      mostPopular: mostPopular,
     };
 
     return dataForTemplate;
--- app/routes/user-details.js
+++ app/routes/user-details.js
@@ -1,12 +1,40 @@
 import route from '@ember/routing/route';
 import { service } from '@ember/service';
+import { action } from '@ember/object';
 
 export default class userdetailsroute extends route {
   @service store;
+  @service currentSession;
 
-  model(params) {
-    return this.store.findRecord('user', params.user_id, {
+  async model(params) {
+    const user = await this.store.findRecord('user', params.user_id, {
       include: ['profile'],
     });
+
+    const isUser = await this.isCurrentUser(params.user_id);
+
+    const dataForTemplate = {
+      user: user,
+      isCurrentUser: isUser,
+    };
+
+    return dataForTemplate;
+  }
+
+  async isCurrentUser(profileUserId) {
+    const currentUser = await this.currentSession.signedInPerson;
+
+    if (!currentUser) return false;
+
+    const currentUserId = currentUser.id;
+
+    if (profileUserId === currentUserId) return true;
+
+    return false;
+  }
+
+  @action
+  refreshModel() {
+    this.refresh();
   }
 }
